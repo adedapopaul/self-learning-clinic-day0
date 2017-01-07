210a43b20c129569673ff0da51c14dc66e4ded85
adedapopaul <adedapopaul@yahoo.com>
2017-01-07 14:06:35 +0100

    Add aritGeo function in the library.js file

--- aritgeo/app/library.js
+++ aritgeo/app/library.js
@@ -1,15 +1,18 @@
 module.exports= {
 
 aritGeo : function (numArray){
-  //find the diifference and ratio between the 1st and 2nd element
-  var ratio =numArray[1] / numArray[0];
+//find the diifference and ratio between the 1st and 2nd element
+  var ratio = numArray[1] / numArray[0];
   var difference = numArray[1] - numArray[0];
   var i = 0;
+  var empty= false;
   var arithmetic = true;
   var geometric= true;
+  var length = numArray.length;
   
-//iterating within the loop to find if the list is arithmeticor geometric
-  for (i=0; i< numArray.length -1 ; i++){
+//iterating within the loop to find if the list is arithmetic or geometric
+  for (i=0; i< length -1 ; i++){
+
     
     //check if the difference between the list is true
       if((numArray[i+1] - numArray[i]) !== difference ){
@@ -21,31 +24,32 @@ aritGeo : function (numArray){
       {
         geometric = false;
       }
-  //check if the list does not have consistent differenceor ratio
+  //check if the list does not have consistent difference or ratio
    if (((numArray[i + 1] / ratio) !== numArray[i]) && ((numArray[i+1]- numArray[i])!== difference)){
      
     arithmetic = false;
     geometric = false;
   }
   
-
-   
+ 
    }
   
-  if(arithmetic === true){
+  if(length=== 0){
+      return 0
+     }
+      else if(geometric === true){
+      
+    return "Geometric";
+      
+    }
+    else if(arithmetic === true){
     return "Arithmetic";
     }
-    else if(geometric === true){
-      
-    return" Geometric";
-      
-    }
+   
     else if (arithmetic ===false  && geometric=== false){
       return -1
     }
-    else if (numArray === []){
-      return '0'
-    }
+    
     
 
 }
